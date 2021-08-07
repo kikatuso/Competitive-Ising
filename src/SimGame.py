7b134c872120b44e0981b0af73299ba16b9f3490
kikatuso <zuzanna.skorniewska@googlemail.com>
2021-08-07 16:46:04 +0100

    implementing numba

--- src/SimGame.py
+++ src/SimGame.py
@@ -3,40 +3,61 @@ import networkx as nx
 import numpy as np
 import math  
 from tqdm import tqdm
-
+from numba import types      # import the types
+from numba.experimental import jitclass
 
 
 from . import helperfunctions as hf
 
-class mf_ising_system:
+
+spec = [
+    ('adj_matrix',types.float64[:]),
+    ('graph_size',types.int32),
+    ('background_field',types.float64[:]),
+    ('fixed_point_iter',types.int32),
+    ('iim_iter',types.int32),
+    ('fp_tol_fac',types.float64),
+    ('optimiser_type',types.string),
+    ('beta1',types.float64),
+    ('beta2',types.float64),
+    ('eps',types.float64)
+]
+
+@jitclass(spec)
+class mf_ising_system(object):
 
     def __init__(self,graph,background_field,fixed_point_iter=int(5*1e4),init_mag='random',fp_tol_fac=1e-5,
                  iim_iter=5000,iim_tol_fac=1e-4,optimiser_type='adam',**kwargs):
         
-        self.graph = graph
-        self.adj_matrix = nx.to_numpy_matrix(graph)
+        self.adj_matrix = nx.to_numpy_matrix(graph,dtype=np.float64)
         self.graph_size = len(graph.nodes.keys())
-        self.background_field = background_field
+        self.background_field = background_field.astype(np.float64)
         self.fixed_point_iter=fixed_point_iter
         self.iim_iter = iim_iter
         self.fp_tol_fac=fp_tol_fac
         self.iim_tol_fac=iim_tol_fac
         self.optimiser_type=optimiser_type
         for k, v in kwargs.items():
-            setattr(self, k, v)
-          
+             setattr(self, k, v)
+
         if init_mag=='random':
             self.init_mag=np.array([np.random.choice([-1,1]) for i in range(self.graph_size)])
-        else:
-            self.init_mag = init_mag 
-   
+        # else:
+        #     self.init_mag = init_mag 
+
+
+    def lr_1(self,x):
+        return np.exp(-x/(0.5*self.iim_iter))
         
+    def lr_2(self,x):
+        return np.exp(-x/(0.5*self.iim_iter))
+   
     def single_mag(self,i,m,beta,field):
         gamma=1.0
         spin_field = np.dot(self.adj_matrix[i],m)
         term = math.tanh(beta*(spin_field+field[i]))
         return (1.0-gamma)*m[i] + gamma*term
-    
+
     def magnetisation(self,mag,beta,field):
         m_old = mag
         m_new = np.zeros(len(m_old))
@@ -64,7 +85,7 @@ class mf_ising_system:
                 mag_d = mag1
  
         return mag_d
-    
+
     def mag_grad(self,beta,mag):
         # Mean Field Susceptibility;
         
@@ -84,7 +105,7 @@ class mf_ising_system:
         control_field = self.control_field_history_pos[it]
         gradient = mag_i_grad
         if self.optimiser_type=='sgd':
-            control_field_update = control_field + self.lr_1(it,self.iim_iter)*gradient
+            control_field_update = control_field + self.lr_1(it)*gradient
         else:
             method = getattr(self,self.optimiser_type)
             control_field_update = control_field + method(mag_i_grad,'pos',it)
@@ -94,7 +115,6 @@ class mf_ising_system:
         
         return control_field_new,gradient
     
-    
     def negative_agent(self,mag_i,it,neg_budget,beta):
         # maximising negative of magnetisation
         mag_i = -1.0*mag_i
@@ -103,7 +123,7 @@ class mf_ising_system:
         control_field = self.control_field_history_neg[it]
         gradient = mag_i_grad
         if self.optimiser_type=='sgd':
-            control_field_update = control_field - self.lr_2(it,self.iim_iter)*gradient
+            control_field_update = control_field - self.lr_2(it)*gradient
         else:
             method = getattr(self,self.optimiser_type)
             control_field_update = control_field -  method(mag_i_grad,'neg',it)
@@ -112,25 +132,25 @@ class mf_ising_system:
         self.control_field_history_neg.append(control_field_new)
         
         return control_field_new,gradient
-    
+
     def sgdm(self,grad,typ,it):
         if typ=='pos':
-            step_size=self.lr_1(it,self.iim_iter)
+            step_size=self.lr_1(it)
             name='changes_pos'
         elif typ=='neg':
-            step_size=self.lr_2(it,self.iim_iter)
+            step_size=self.lr_2(it)
             name='changes_neg'
 
         new_change = step_size * grad + self.momentum * getattr(self,name)[-1]
         getattr(self,name).append(new_change)
         return new_change
-     
+
     def adagrad(self,grad,typ,it):
         if typ=='pos':
-            step_size=self.lr_1(it,self.iim_iter)
+            step_size=self.lr_1(it)
             name='grad_sums_pos'
         elif typ=='neg':
-            step_size=self.lr_2(it,self.iim_iter)
+            step_size=self.lr_2(it)
             name='grad_sums_neg'
         grad_sums = getattr(self,name) 
         grad_sums += grad**2
@@ -138,7 +158,7 @@ class mf_ising_system:
         change = alpha*grad
         setattr(self,name,grad_sums)
         return change
-    
+
     def adadelta(self,grad,typ,it,ep=1e-4):
         if typ=='pos':
             rho=self.rho1
@@ -161,11 +181,11 @@ class mf_ising_system:
     
     def adam(self,grad,typ,it):
         if typ=='pos':
-            lr=self.lr_1(it,self.iim_iter)
+            lr=self.lr_1(it)
             name1='ms_pos'
             name2='vs_pos'
         elif typ=='neg':
-            lr=self.lr_2(it,self.iim_iter)
+            lr=self.lr_2(it)
             name1='ms_neg'
             name2='vs_neg'
         ms_previous = getattr(self,name1)
@@ -183,7 +203,6 @@ class mf_ising_system:
         change = lr* mhat/(np.sqrt(vhat) + self.eps)
         return change
     
-
     def second_partial_dffs(self,mag_ii,tot_field,beta,a=1e-5):
         ' estimating 2nd partial derivative with respect to each agents external field' 
         
@@ -211,7 +230,7 @@ class mf_ising_system:
         curv_player_pos = curv_player_neg
         self.grad2pos.append(curv_player_pos)
         return np.array([curv_player_pos,curv_player_neg])
-    
+
     def init_optimiser(self):
         if self.optimiser_type=='sgdm':
             self.changes_pos = [np.zeros(self.graph_size)]
@@ -227,7 +246,6 @@ class mf_ising_system:
             self.ms_neg = np.zeros(self.graph_size)
             self.vs_neg = np.zeros(self.graph_size)
 
-        
     def MF_IIM(self,pos_budget,neg_budget,beta,init_alloc='random',progress=True):
 
         if isinstance(init_alloc,(np.ndarray, np.generic)):
